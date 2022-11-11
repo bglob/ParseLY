f28a88cd69d6a0a26fee32084da640c5ae174626
Melissa Crozier <mgckq8@mail.missouri.edu>
2022-11-11 17:33:54 -0600

    Rewrote main to take a file as an argument

--- main.py
+++ main.py
@@ -1,56 +1,17 @@
 import sys
 from antlr4 import *
+from antlr4.tree.Trees import Trees
 from dist.MyGrammerLexer import MyGrammerLexer
 from dist.MyGrammerParser import MyGrammerParser
 from dist.MyGrammerVisitor import MyGrammerVisitor
 
-
-def get_username():
-    from pwd import getpwuid
-    from os import getuid
-    return getpwuid(getuid())[ 0 ]
-
-  
-class MyVisitor(MyGrammerVisitor):
-    def visitNumberExpr(self, ctx):
-        value = ctx.getText()
-        return int(value)
-
-    def visitParenExpr(self, ctx):
-        return self.visit(ctx.expr())
-
-    def visitInfixExpr(self, ctx):
-        l = self.visit(ctx.left)
-        r = self.visit(ctx.right)
-
-        op = ctx.op.text
-        operation =  {
-        '+': lambda: l + r,
-        '-': lambda: l - r,
-        '*': lambda: l * r,
-        '/': lambda: l / r,
-        '%': lambda: l % r,
-        }
-        return operation.get(op, lambda: None)()
-
-    def visitByeExpr(self, ctx):
-        print(f"goodbye {get_username()}")
-        sys.exit(0)
-
-    def visitHelloExpr(self, ctx):
-        return (f"{ctx.getText()} {get_username()}")
-
-
+def main(argv):
+    io_stream = FileStream(argv[1])
+    lexer = MyGrammerLexer(io_stream)
+    stream = CommonTokenStream(lexer)
+    parser = MyGrammerParser(stream)
+    tree = parser.start() 
+    print(Trees.toStringTree(tree, None, parser))
+    
 if __name__ == "__main__":
-    while 1:
-        data =  InputStream(input(">>> "))
-        # lexer
-        lexer = MyGrammerLexer(data)
-        stream = CommonTokenStream(lexer)
-        # parser
-        parser = MyGrammerParser(stream)
-        tree = parser.expr()
-        # evaluator
-        visitor = MyVisitor()
-        output = visitor.visit(tree)
-        print(output)
+    main(sys.argv)
