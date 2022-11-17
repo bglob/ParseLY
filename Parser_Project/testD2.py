71550511f5d1e9e11c8df2f493477aefe0e3937d
RajuNichols <nichols.raju@gmail.com>
2022-11-17 14:33:05 -0600

    Added more nested if's

--- Parser_Project/testD2.py
+++ Parser_Project/testD2.py
@@ -6,6 +6,8 @@ if test8<=10:
         test8=7
     if test8 >= 2:
         hi = "hello"
+        if test8 == 3:
+            hi = "test"
     else:
         hi = "world"
 
@@ -13,4 +15,3 @@ if test9==True and not test10 or test11:
     test9=False
 else:
     test9=True
-  
